82e8d3ceac173c9ebd46ac66a6f8b049eeb33b18
donaldmashiri <donaldtondemashiri@gmail.com>
2023-05-17 10:38:10 +0200

    gg

---
+++ database/seeders/Userseeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class Userseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::where('email', 'admin@admin.com')->first();
+
+        if(!$user){
+            User::create([
+                'role' => 'admin',
+                'name' => 'Donnie Angel',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password')
+            ]);
+        }
+
+    }
+}
